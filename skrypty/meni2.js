932d41f6f5b149f688824452c3e3e59e110e669c
TCM95 <teceem95@gmail.com>
2026-08-16 17:54:57 +0200

    Update meni2.js

--- skrypty/meni2.js
+++ skrypty/meni2.js
@@ -12,7 +12,7 @@
     'use strict';
 
     const scripts = [
-        { name: "Wybierz skrypt ▼", run: () => {} },
+        { name: "   ▼   ", run: () => {} },
         { name: "Health Check", run: () => $.getScript('https://twscripts.dev/scripts/defenseHealthCheck.js') },
         { name: "Przegląd ataków", run: () => { window.NOBLE_GAP = 100; window.FORMAT = '%unit% | %sent%'; $.getScript('https://twscripts.dev/scripts/incomingsOverview.js'); } },
         { name: "Filtry raportów", run: () => $.getScript('https://twscripts.dev/scripts/advancedReportFilters.js') },
